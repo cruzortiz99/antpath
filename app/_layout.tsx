a032503f7278d4e84bd061eb0ec0e288b1d0ae0f
cruzortiz <cruzortiz099@gmail.com>
2025-08-20 13:34:26 -0400

    - adding request helper.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import "@/assets/css/global.css";
 import { OnboardingStore } from "@/lib/data_sources/stores/onboarding-store";
 import { useObservable } from "@/lib/utils/observables";
 import { Stack } from "expo-router";
+
 export default function RootLayout() {
   const [showOnboarding] = useObservable(() => OnboardingStore.instance, []);
   return (
--- lib/data_source/db/index.tsx
+++ lib/data_source/db/index.tsx
@@ -1,4 +1,4 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import * as SQLite from "expo-sqlite";
 const expoSQL = SQLite.openDatabaseSync("db.sql");
-export const db = drizzle(expoSQL);
+export const DB = drizzle(expoSQL);
